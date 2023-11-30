652623a0eb5e6e17fc5b90f2aa4a117ed89dc6f1
MICHEL Pierre <pierre.michel.clc54@gmail.com>
2023-11-30 18:13:17 +0100

    Delete Image Folder and Download IMG
    Delete Image Folder (bug with folder path) and copy IMG to the current folder (easier path)
    Interface.py on progress

--- Interface.py
+++ Interface.py
@@ -1,8 +1,29 @@
 from tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 window = Tk()
 window.title("Interface Nageur")
-window.minsize("1000", "800")
+window.minsize(1000, 800)
+
+new_window = ttk.Notebook(window)
+first_window = ttk.Frame(new_window)
+second_window = ttk.Frame(new_window)
+new_window.add(first_window, text="Program")
+new_window.add(second_window, text="Result")
+
+# Ajout d'un canevas à l'onglet n°2
+canvas_second_window = Canvas(second_window)
+canvas_second_window.pack(fill = "both", expand = True)
+
+# Charger l'image à utiliser comme fond avec Pillow
+image_path = "Projet_Tkinter/IMG.png"
+background_image_pil = Image.open(image_path)
+background_image = ImageTk.PhotoImage(background_image_pil)
+
+# Créer l'image de fond dans le canevas
+canvas_second_window.create_image(0, 0, image=background_image)
+
+new_window.pack(expand=1, fill="both")
 
 window.mainloop()
